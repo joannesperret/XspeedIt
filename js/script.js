fa512a83ee8cde8a5e3a26514fe43233b25e216c
Joannes <joannesperret@hotmail.fr>
2021-03-28 10:42:38 +0200

    Initialisation du regroupement de 3 séquences arts

--- js/script.js
+++ js/script.js
@@ -1,217 +1,95 @@
-// fonction de récupération du INPUT
-// @param: int
-// Déclaration des variables globales
-
-
-
 function init() {
 
+    // déclaration des variables globales
 
-// script conversion Romaine en nombre
+    let chaineArticles = new Array;
+    let colis = new Array;
 
-// le clic sur la Div Btn déclenche le script
+    // script optimisation colisage articles
 
-   document.getElementById("divBtn").onclick = convertir;
-   
-// récupération de la valeur saisie
+    // le clic sur la Div Btn déclenche le script
 
-    var saisie = document.getElementById("in").value;
-    // var result="";
+    document.getElementById("divBtn").onclick = afficher;
 
-// fonction de récupération de la valeur saisie
-    function getValue() {
-        document.getElementById("in").value;
-    }
+    // fonction de génération d'une chaine d'articles
 
+    function generateArticles() {
+        chaineArticles = [];
+        for (i = 0; i < 15; i++) {
+            let randomColis;
+            randomColis = Math.floor((Math.random() * (10 - 1) + 1));
+            chaineArticles.push(randomColis);
+        }
+    };
 
+    // fonction de colisage initiale
 
-// fonction d'affichage
+    function makeColis() {
+        let i = 0;
+        colis = [];
+        while (i < chaineArticles.length) {
+            if (chaineArticles[i] + (chaineArticles[i + 1]) + (chaineArticles[i + 2]) <= 10) {
+                 console.log('colis de 3 séquences');
+                // console.log('i = ' + chaineArticles[i]);
+                // console.log('i + 1 = ' + chaineArticles[i + 1]);
+                // console.log(chaineArticles);
+                colis.push(chaineArticles[i] + '' + chaineArticles[i + 1] + '' + chaineArticles[i + 1] + '/');
+                i = i+3;
+            }
+            if (chaineArticles[i] + (chaineArticles[i + 1]) > 10) {
+                // console.log('colis trop rempli');
+                // console.log('i = ' + chaineArticles[i]);
+                // console.log('i + 1 = ' + chaineArticles[i + 1]);
+                // console.log(chaineArticles);
+                colis.push(chaineArticles[i] + '/')
+                i++;
+            }
+            else {
+                if (chaineArticles[i + 1] != undefined) {
+
+                    // console.log('i = ' + chaineArticles[i]);
+                    // console.log('i + 1 = ' + chaineArticles[i + 1])
+                    colis.push(chaineArticles[i] + '' + chaineArticles[i + 1] + '/');
+                    // console.log('colis' + colis);
+                    // console.log(chaineArticles);
+                    // console.log('on continue le remplissage');
+                    i = i + 2;
+                } else {
+                    colis.push(chaineArticles[i]);
+                };
+            };
+        };
+    };
+
+    // fonction d'affichage
+
 
     function afficher() {
-        var pResultats = document.getElementById("pResultats");
-        pResultats.innerHTML = (saisie + " s'écrit: " + unit + " en nombres.");
+
+        generateArticles();
+
+        let affichageChaineArticles = document.getElementById("affichageChaineArticles");
+        affichageChaineArticles.innerHTML = chaineArticles.join('').slice(0, -1);
+        console.log('dernier caractère à supprimer')
+
+        makeColis();
+
+        let affichageColis = document.getElementById("affichageColis");
+
+        if ((colis.join('').substr(-1, 1)) === "/") {
+            affichageColis.innerHTML = colis.join('').slice(0, -1);
+        }
+        else {
+            affichageColis.innerHTML = colis.join('');
+            console.log('Précolisage ' + colis);
+        }
+        console.log('Colis ' + colis);
+        console.log('Détail articles ' + chaineArticles);
+
+        let affichageNbColis = document.getElementById("affichageNbColis");
+        affichageNbColis.innerHTML = colis.length + ' cartons utilisés.';
     }
-    ;
 
-    // Fonction de conversion des unités Romaines en chiffres
-    function unit() {
-        // récupération de la valeur saisie
-        var input = document.getElementById("in").value;
-        // récupération de l'objet HTML d'affichage du résultat
-        var pResultats = document.getElementById("pResultats");
-        // déclaration des variables
-        // unité
-        var unitRom = 0;
-        // récupération du dernier caractère de la saisie
-        var unit1 = (input.substr(-1, 1));
-        switch (unit1) {
-
-            case 'I':
-                unitRom = 1;
-                break;
-            case 'V':
-                unitRom = 5;
-                break;
-//            case 'X':
-//                unitRom = 10;
-//                break;
-        }
-        // récupération des deux derniers caractères de la saisie               
-        var unit2 = (input.substr(-2));
-        switch (unit2) {
-            case 'II':
-                unitRom = 2;
-                break;
-            case 'IV':
-                unitRom = 4;
-                break;
-            case 'VI':
-                unitRom = 6;
-                break;
-            case 'IX':
-                unitRom = 9;
-                break;
-        }
-
-        // récupération des deux derniers caractères de la saisie
-        var unit3 = (input.substr(-3));
-        switch (unit3) {
-            case 'III':
-                unitRom = 3;
-                break;
-            case 'VII':
-                unitRom = 7;
-                break;
-            case 'IIX':
-                unitRom = 8;
-                break;
-        }
-    
-        console.log("unit1: "+unit1);
-        
-        console.log("unit3: "+unit3);
-        console.log("unit2: "+unit2);
-        console.log("unit1: "+unit1);
-        console.log("unitRom: "+unitRom);
- 
-         
-        
-        console.log("unitRom: "+unitRom);
-        return unitRom; 
-    };
-    
-       function ten(){
-        // récupération de la valeur saisie
-        var input = document.getElementById("in").value;
-       // récupération de l'objet HTML d'affichage du résultat
-        var pResultats = document.getElementById("pResultats"); 
-       
-        // contrôle de la présence des centaines dans la saisie 
-         // centaine
-        var dizaine = new Object();  
-        var tenRom = 0;         
-        
-        dizaine["XXX"] = 30;
-        dizaine["XL"] = 40;           
-        dizaine["LXX"] = 70;
-        dizaine["LX"] = 60;
-        dizaine["L"] = 50;
-        dizaine["XXC"] = 80;
-        dizaine["XC"] = 90;
-        dizaine["XX"] = 20;
-        dizaine["X"] = 10;
-        
-        for (var i in dizaine)
-        {
-            if(input.includes(i)){tenRom = dizaine[i];
-            break;
-        }
-            
-        }
-        
-        return tenRom; 
-    };  
-    
-        function cent(){
-        // récupération de la valeur saisie
-        var input = document.getElementById("in").value;
-       // récupération de l'objet HTML d'affichage du résultat
-               
-        // contrôle de la présence des centaines dans la saisie 
-         // centaine
-        var centaine = new Object();  
-        var centRom = 0;         
-        
-        centaine["CCC"] = 300;
-        centaine["CD"] = 400;           
-        centaine["DCC"] = 700;
-        centaine["DC"] = 600;
-        centaine["D"] = 500;
-        centaine["CCM"] = 800;
-        centaine["CM"] = 900;
-        centaine["CC"] = 200;
-        centaine["C"] = 100;
-        
-        for (var i in centaine)
-        {
-            if(input.includes(i)){centRom = centaine[i];
-            break;
-        }
-            
-        }
-        
-        return centRom; 
-    };  
-    
-       function mille(){
-        // récupération de la valeur saisie
-        var input = document.getElementById("in").value;
-       // récupération de l'objet HTML d'affichage du résultat
-               
-        // contrôle de la présence des milliers dans la saisie 
-        
-        var milliers = new Object();  
-        var koRom = 0;         
-        
-        milliers["MMMM"] = 4000;
-        milliers["MMM"] = 3000;           
-        milliers["MM"] = 2000;
-        milliers["M"] = 1000;      
-        
-        for (var i in milliers)
-        {
-            if(input.includes(i)){koRom = milliers[i];
-            break;
-        }
-            
-        }
-        
-        return koRom; 
-        
-    }; 
-   
-   function convertir(){
-       //saisie= getValue;
-       var saisie= document.getElementById("in").value;
-       var resultUnit= unit();
-       var resultTen= ten();
-       var resultCent= cent();
-       var resultKo= mille();
-       var pResultats = document.getElementById("pResultats"); 
-       var result= resultUnit + resultTen+ resultCent + resultKo;
-       console.log("saisie: " + saisie);
-       console.log("resultat unite: " + resultUnit);
-       console.log("resultat dizaine: " + resultTen);
-       console.log("resultat centaines: " + resultCent);
-       console.log("resultat milliers: " + resultKo);
-       console.log("somme: " + result);
-       if(result===0){pResultats.innerHTML = ("Vous essayez de traduire des hiéroglyphes? ;) ");
-       }
-       else{
-       pResultats.innerHTML = (saisie + " s'écrit: " + result + " en nombres.");
-      }
-    };
-   
 };
 
 /// init
